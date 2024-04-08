75e3ad25644da2d42bde239573046bd643c160ab
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2024-04-08 14:57:46 +0800

    sample lambda code

--- functions/lambda_inference/index.py
+++ functions/lambda_inference/index.py
@@ -1,5 +1,63 @@
+import pickle
+import boto3
+import json
+import nltk
+from nltk.tokenize import TreebankWordTokenizer
 
+def lambda_handler(event, context):
+    
+    text = "Input Text"
+    bucket = "jack-bucket-ml-code"
+    vectoriser_file = ""
+    
+    s3 = boto3.resource('s3')
+    lambda_ = boto3.client('lambda')
+
+    nltk.download('punkt')
+    nltk.download('stopwords')
+
+    vectoriser = pickle.loads(
+        s3.Bucket(bucket)\
+            .Object(f"{vectoriser_file}.pickle")\
+            .get()['Body'].read()
+    )
+    preprocessed_text = vectoriser.transform([text])
+
+
+    lambda_name = ""
+    processed_text = json.dumps({"text_vector": preprocessed_text})
+
+    response = lambda_.invoke(
+        FunctionName=lambda_name,
+        Payload=processed_text,
+    )
+    print(response)
+    return response["Payload"]
+
+
+import pickle
+import boto3
+import sklearn
 
 def lambda_handler(event, context):
     print(event)
-    return event
+    bucket = "jack-bucket-ml-code"
+    model_file = ""
+    
+    s3 = boto3.resource('s3')
+
+    model = pickle.loads(
+        s3.Bucket(bucket)\
+            .Object(f"{model_file}.pickle")\
+            .get()['Body'].read()
+    )
+
+    preprocessed_text = event["text_vector"]
+    prediction = model.predict(preprocessed_text)
+
+    if prediction == 1:
+        response = '\nYOUR TEXT SEEMS THREATENING!\n'
+    else:
+        response = '\nYOUR TEXT SEEMS NON-THREATENING!\n'
+    
+    return {"response": response}
